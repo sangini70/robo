1c925e66bc6eaafcac145cb88f40ab8b096b491b
rubeon <luganopizza@gmail.com>
2026-03-23 17:22:59 +0900

    Update index.ts

--- src/posts/index.ts
+++ src/posts/index.ts
@@ -1,5 +1,7 @@
 import { roboAdvisor } from "./robo-advisor";
+import { goldInvestmentGuide } from "./gold-investment-guide";
 
 export const posts = [
-  roboAdvisor
+  roboAdvisor,
+  goldInvestmentGuide
 ];
