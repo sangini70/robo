03b140d6c2e6fb9cdfb54e51afdf1367db195463
rubeon <luganopizza@gmail.com>
2026-03-23 16:52:03 +0900

    Update PostPage.tsx

--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,1 +1,9 @@
+import roboAdvisorHtml from "../posts/robo-advisor";
 
+export default function PostPage() {
+  return (
+    <main style={{ padding: "40px 20px" }}>
+      <div dangerouslySetInnerHTML={{ __html: roboAdvisorHtml }} />
+    </main>
+  );
+}
